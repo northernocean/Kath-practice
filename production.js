570f391ebca9de8a98e588482b7d6d9c26cbe583
Lopokoiyit <104138775+Lopokoiyit@users.noreply.github.com>
2022-09-08 21:15:30 +1000

    Update production.js

--- production.js
+++ production.js
@@ -1,24 +1,21 @@
-
-
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#app")
+const svg = d3.select("#production")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
-d3.csv("Production+Consumption"), function(data) 
+d3.csv("production+consumption").then( function(data) {
 
     // List of groups (here I have one group per column)
-    {var allGroup = d3.map(data, function(d){return(d.state)}).keys()
+    const allGroup = new Set(data.map(d => d.state))
 
     // add the options to the button
     d3.select("#selectButton")
@@ -30,33 +27,33 @@ d3.csv("Production+Consumption"), function(data)
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
     // A color scale: one color for each group
-    var myColor = d3.scaleOrdinal()
+    const myColor = d3.scaleOrdinal()
       .domain(allGroup)
       .range(d3.schemeSet2);
 
     // Add X axis --> it is a date format
-    var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return d.year_id-2; }))
+    const x = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return d.year_id; }))
       .range([ 0, width ]);
     svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", `translate(0, ${height})`)
       .call(d3.axisBottom(x).ticks(7));
 
     // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.energy_consumption_pj; })])
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.energy_production_gwh; })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
 
     // Initialize line with first group of the list
-    var line = svg
+    const line = svg
       .append('g')
       .append("path")
-        .datum(data.filter(function(d){return d.state==allGroup[0]}))
+        .datum(data.filter(function(d){return d.state=="VIC"}))
         .attr("d", d3.line()
-          .x(function(d) { return x(d.year_id-2) })
-          .y(function(d) { return y(+d.energy_consumption_pj) })
+          .x(function(d) { return x(d.year_id) })
+          .y(function(d) { return y(+d.energy_production_gwh) })
         )
         .attr("stroke", function(d){ return myColor("valueA") })
         .style("stroke-width", 4)
@@ -66,7 +63,7 @@ d3.csv("Production+Consumption"), function(data)
     function update(selectedGroup) {
 
       // Create new data with the selection?
-      var dataFilter = data.filter(function(d){return d.state==selectedGroup})
+      const dataFilter = data.filter(function(d){return d.state==selectedGroup})
 
       // Give these new data to update line
       line
@@ -74,14 +71,18 @@ d3.csv("Production+Consumption"), function(data)
           .transition()
           .duration(1000)
           .attr("d", d3.line()
-            .x(function(d) { return x(d.year_id-2) })
-            .y(function(d) { return y(+d.energy_consumption_pj) })
+            .x(function(d) { return x(d.year_id) })
+            .y(function(d) { return y(+d.energy_production_gwh) })
           )
-          .attr("stroke", function(d){ return myColor(selectedGroup) })}
+          .attr("stroke", function(d){ return myColor(selectedGroup) })
+    }
 
     // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) 
+    d3.select("#selectButton").on("change", function(event,d) {
         // recover the option that has been chosen
-        {var selectedOption = d3.select(this).property("value")
+        const selectedOption = d3.select(this).property("value")
         // run the updateChart function with this selected option
-        update(selectedOption)})}
+        update(selectedOption)
+    })
+
+})
